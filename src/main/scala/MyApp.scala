f014090b0e2e9865e89b9605a83f80f31db88f49
Hasan Yahya <hasanyah@gmail.com>
2021-11-10 01:17:10 +0100

    Add filter/selection methods Refactor code Test Pipeline

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -12,8 +12,64 @@ import org.apache.spark.ml.feature.Normalizer
 import org.apache.spark.ml.feature.OneHotEncoder
 import org.apache.spark.ml.feature.StringIndexer
 import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.feature.UnivariateFeatureSelector
+import org.apache.spark.ml.feature.VarianceThresholdSelector
+import org.apache.spark.ml.Pipeline
 
 object MyApp {
+
+	/*
+	** These columns have been forbidden by the assignment rules
+	** as these values are unknown at the time of flight departure
+	*/
+	val ForbiddenColumns = Array(
+		"ArrTime",
+		"ActualElapsedTime",
+		"AirTime",
+		"TaxiIn",
+		"Diverted",
+		"CarrierDelay",
+		"WeatherDelay",
+		"NASDelay",
+		"SecurityDelay",
+		"LateAircraftDelay"
+	)
+
+	/*
+	** The following columns are exluded because
+	** they were either mostly null or did not have an effect on the resulting delay
+	*/
+	val ExcludedColumns = Array(
+		"Cancelled",
+		"CancellationCode",
+		"TailNum",
+		"DayOfWeek",
+		"TaxiOut"
+	)
+		
+	// Numerical Variables
+	val IntColumns = Array(
+		"Year",
+		"Month",
+		"DayOfMonth",
+		"DepTime",
+		"CRSDepTime",
+		"CRSArrTime",
+		"FlightNum",
+		"CRSElapsedTime",
+		"DepDelay",
+		"Distance"
+	)
+
+	// Categorical variables
+	val CatColumns = Array(
+		"Origin",
+		"Dest",
+		"UniqueCarrier"
+	)
+
+	val LabelColumn = "ArrDelay"
+
 	def main(args : Array[String]) {
 		Logger.getLogger("org").setLevel(Level.WARN)
 		
@@ -31,56 +87,7 @@ object MyApp {
 
 		// Check if file can be read without an issue
 		
-		/*
-		** These columns have been forbidden by the assignment rules
-		** as these values are unknown at the time of flight departure
-		*/
-		val FORBIDDEN_COLUMNS = Array(
-				"ArrTime",
-				"ActualElapsedTime",
-				"AirTime",
-				"TaxiIn",
-				"Diverted",
-				"CarrierDelay",
-				"WeatherDelay",
-				"NASDelay",
-				"SecurityDelay",
-				"LateAircraftDelay"
-		)
-
-		/*
-		** The following columns are exluded because
-		** they were either mostly null or did not have an effect on the resulting delay
-		*/
-		val EXCLUDED_COLUMNS = Array(
-			"Cancelled",
-			"CancellationCode",
-			"TailNum",
-			"DayOfWeek",
-			"TaxiOut"
-		)
-
-		// Numerical Variables
-		val INT_COLUMNS = Array(
-			"Year",
-			"Month",
-			"DayOfMonth",
-			"DepTime",
-			"CRSDepTime",
-			"CRSArrTime",
-			"FlightNum",
-			"CRSElapsedTime",
-			"ArrDelay",
-			"DepDelay",
-			"Distance"
-		)
-
-		// Categorical variables
-		val CAT_COLUMNS = Array(
-			"Origin",
-			"Dest",
-			"UniqueCarrier"
-		)
+		
 		
 		val spark = org.apache.spark.sql.SparkSession
 			.builder()
@@ -103,11 +110,11 @@ object MyApp {
 		data = data.filter("Cancelled == 0")
 
 		// Drop all excluded columns	
-		data = data.drop((FORBIDDEN_COLUMNS++EXCLUDED_COLUMNS): _*)
+		data = data.drop((ForbiddenColumns++ExcludedColumns): _*)
 
 		// TODO: Use the string columns (Origin, destination, UniqueCarrier) by converting them to categorical variables and find a way to utilize them. 
 		val indexer = new StringIndexer()
-			.setInputCols(CAT_COLUMNS)
+			.setInputCols(CatColumns)
 			.setOutputCols(Array("Ind_Origin","Ind_Dest","Ind_UniqueCarrier"))
 		val indexed = indexer.fit(data).transform(data)
 
@@ -118,7 +125,7 @@ object MyApp {
 		data = encoded
 
 		// Cast int columns to int
-		for (colName <- INT_COLUMNS)
+		for (colName <- IntColumns++Array(LabelColumn))
 			data = data.withColumn(colName, col(colName).cast("integer"))
 
 		// TODO: remove the 4 lines below before the final submission
@@ -127,49 +134,89 @@ object MyApp {
 		val stringData = data.collect().mkString(" ")
 		new PrintWriter("csv_output") { write("NumberOfTotalRows="+count+"\n"+stringData+"\n"); close }
 
-		// TODO: Check each variable one by one to determine the relationship with the result of the machine learning algorithm
-		
-		
-		// TODO: Cross validation for the machine learning output
-
-
-
 		// --------------------------------------------------------------------------------------------------
 		// Machine learning begins
 		// --------------------------------------------------------------------------------------------------
 
-		val columns = data.columns.toSeq
-		val assembler = new VectorAssembler()
-  			//.setInputCols(Array(columns: _*))
-			.setInputCols(Array("Year", "Month", "DayOfMonth", "DepTime", "CRSDepTime", "CRSArrTime", "FlightNum", "CRSElapsedTime", "ArrDelay", "DepDelay","Distance", "Enc_Origin","Enc_Dest","Enc_UniqueCarrier"))
-  			.setOutputCol("features")
-		
-		val output = assembler.setHandleInvalid("skip").transform(data)
-			//output.show(truncate=false)
+		//SPLITING DATA
+		val split = data.randomSplit(Array(0.8,0.2))
+		val trainingData = split(0)
+		val testData = split(1)
 
+		// TODO: Check each variable one by one to determine the relationship with the result of the machine learning algorithm
+		applyLinearRegressionModel(trainingData);
+
+		// TODO: Cross validation for the machine learning output
+
+	}
+
+	def applyUnivariateFilter( data:DataFrame ) : Unit = {
+		val assembler = new VectorAssembler()
+			.setInputCols(IntColumns++Array("Enc_Origin","Enc_Dest","Enc_UniqueCarrier"))
+			.setOutputCol("features")
+			.setHandleInvalid("skip")
+		
+		println("Output of assembler")
+		val output = assembler.transform(data)
+
+		val selector = new UnivariateFeatureSelector()
+			.setFeatureType("continuous")
+			.setLabelType("categorical")
+			.setSelectionMode("numTopFeatures")
+			.setSelectionThreshold(10)
+			.setFeaturesCol("features")
+			.setLabelCol(LabelColumn)
+			.setOutputCol("selectedFeatures")
+
+		val result = selector.fit(output).transform(output)
+
+		println(s"UnivariateFeatureSelector output with top ${selector.getSelectionThreshold}" +
+		s" features selected using f_classif")
+		result.select("selectedFeatures").show()
+   }
+
+   def applyVarianceThresholdSelector( data:DataFrame ) : Unit = {
+		val selector = new VarianceThresholdSelector()
+			.setVarianceThreshold(2.0)
+			.setFeaturesCol("features")
+			.setOutputCol("selectedFeatures")
+
+		val result = selector.fit(data).transform(data)
+
+		println(s"Output: Features with variance lower than" +
+		s" ${selector.getVarianceThreshold} are removed.")
+		result.show()
+   }
+
+   def applyLinearRegressionModel( data:DataFrame ) : Unit = {
+	   val assembler = new VectorAssembler()
+  			.setInputCols(IntColumns++Array("Enc_Origin","Enc_Dest","Enc_UniqueCarrier"))
+			.setOutputCol("features")
+			.setHandleInvalid("skip")
+		
+		// println("Output of assembler")
+		val output = assembler.transform(data)
+		// output.show(truncate=false)
+		
 		//NORMALIZATION
 		val normalizer = new Normalizer()
   			.setInputCol("features")
   			.setOutputCol("normFeatures")
   			.setP(1.0)
 
+		// println("Output of normalizer")
 		val l1NormData = normalizer.transform(output)
-		//l1NormData.show(truncate=false)
-
-		//SPLITING DATA
-		val split = l1NormData.randomSplit(Array(0.8,0.2))
-		val trainingData = split(0)
-		val testData = split(1)
+		// l1NormData.show(truncate=false)
 
 		val lr = new LinearRegression()
-  			.setFeaturesCol("features")
+  			.setFeaturesCol("normFeatures")
   			.setLabelCol("ArrDelay")
   			.setMaxIter(10)
   			.setElasticNetParam(0.8)
 		
-		//val lrModel = lr.fit(output)
-		val lrModel = lr.fit(trainingData)
-		//val lrModel = lr.fit(l1NormData)
+		//val lrModel = lr.fit(trainingData)
+		val lrModel = lr.fit(l1NormData)
+		
 		println(s"Coefficients: ${lrModel.coefficients}")
 		println(s"Intercept: ${lrModel.intercept}")
 		val trainingSummary = lrModel.summary
@@ -178,6 +225,29 @@ object MyApp {
 		trainingSummary.residuals.show()
 		println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
 		println(s"r2: ${trainingSummary.r2}")
-	}
+   }
+
+   def applyLinearRegressionModelViaPipeline( trainingData:DataFrame, testData:DataFrame ) : Unit = {
+	   val assembler = new VectorAssembler()
+  			.setInputCols(IntColumns++Array("Enc_Origin","Enc_Dest","Enc_UniqueCarrier"))
+			.setOutputCol("features")
+			.setHandleInvalid("skip")
+		
+		val normalizer = new Normalizer()
+  			.setInputCol("features")
+  			.setOutputCol("normFeatures")
+  			.setP(1.0)
+
+		val lr = new LinearRegression()
+  			.setFeaturesCol("normFeatures")
+  			.setLabelCol("ArrDelay")
+  			.setMaxIter(10)
+  			.setElasticNetParam(0.8)
+		
+		val pipeline = new Pipeline()
+			.setStages(Array(assembler, normalizer, lr))
+		val model = pipeline.fit(trainingData)
+		println("Output of pipeline model for test data")
+		model.transform(testData).show(truncate=false)
+   }
 }
-    
